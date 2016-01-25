987fe56f6f8d529e87f9f764d96aac3c2f48f97b
simonbaumann <simonbaumann20@gmail.com>
2016-01-25 10:03:40 +0100

    Enable hot reload for non-localhost

---
+++ hotReload.js
@@ -0,0 +1,8 @@
+
+
+
+setTimeout(() => {
+	const devServer = document.createElement('script');
+	devServer.setAttribute('src', 'http://${window.location.host}/webpack-dev-server.js');
+	document.body.appendChild(devServer);	
+});
