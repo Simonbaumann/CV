f5ff319f263a29358e6f58b4e52dfd57a0c9b812
Simonbaumann <simonbaumann20@gmail.com>
2016-01-25 15:36:51 +0000

    hotReload file modified

--- hotReload.js
+++ hotReload.js
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
 setTimeout(() => {
 	const devServer = document.createElement('script');
-<<<<<<< HEAD
 	devServer.setAttribute('src', `http://${window.location.host}/webpack-dev-server.js`);
-=======
-	devServer.setAttribute('src', 'http://${window.location.host}/webpack-dev-server.js');
->>>>>>> 290703de3078bd5be0a39347178e8671831c03c1
-=======
-
-
-
-setTimeout(() => {
-	const devServer = document.createElement('script');
-	devServer.setAttribute('src', 'http://${window.location.host}/webpack-dev-server.js');
->>>>>>> 987fe56f6f8d529e87f9f764d96aac3c2f48f97b
 	document.body.appendChild(devServer);	
 });
